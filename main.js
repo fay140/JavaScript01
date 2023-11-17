d2c8eabb4110687f6fb98d7742a389a06f1c3782
fay140 <tu4155292@taibahu.edu.sa>
2023-11-17 23:28:21 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,81 @@
+console.log("Welcome to the JavaScript course");
+let color = 'blue'; console.log(color); 
+console.log(color);
+ console.log(color);
+  console.log(color);
+   console.log(color); color = 'black';
+    console.log(color); 
+    var_name = 'Ali'; 
+const yearOfBirth = 1995; 
+console.log(yearOfBirth);
+ age = 26;
+  console.log(age);
+   let result = 2 < 3;
+    console.log(result);
+    console.log(typeof result);
+    let message = 'Welcome to JavaScript'; 
+    console.log(typeof message);
+     console.log(message);
+     let name; 
+     console.log(name);
+     let value = -2.5;
+     console.log(value);
+     ///let value = null;
+///console.log(value);
+let language = 'JavaScript';
+ console.log(language.length);
+ //let language 'JavaScript'; 
+ //let message = 'Welcome to ' + language; 
+ //console.log(message);
+ //let color = 'red'; 
+let colors = ['red', 'green', 'blue']; 
+console.log(colors[0]);
+//let message = ["Welcome', 'to', 'JavaScript'); 2 let result = message.join('@'); 3 console.log(result);
+let number = 1 
+ if (number > 0) {
+    console, log('+'); 
+ }else if (number < 0) {
+    console.log('-'); 
+ }else{  console.log('zero'); 
+  }
+  const colors = ['red', 'green', 'blue']; 
+ let colorNumber = 2; 
+ switch(colorNumber){
+    case 1: 
+    console.log(colors[0]); 
+    break; 
+      case 2: console.log(colors [1]);
+      break;
+    case 3: console.log(colors [2]);}
+    const colors = ['red', 'green', 'blue', 'black'];
+     for(let i = 0; i < colors.length; i++){
+         console.log(colors[i]); }
+         const colors = ['red', 'green', 'blue']; 
+          for(let color of colors){
+             console.log(color);}
+
+             const numbers = [1, 2, 3, 0, 4, 5]; 
+              for(let i = 0; i < numbers.length; i++){
+                 if (numbers[i] ==0){
+                     continue; } 
+                     console.log(numbers [i]);  } 
+                      console.log('*');
+                      function printNumbers(){ 
+                        console.log(1); 
+                         console.log(2); 
+                          console.log(3); 
+                        } 
+                         printNumbers();
+                         function printNumbers (to){
+                             for (let i = 1; i <= to; i++) {
+                                 console.log(i); 
+                                }
+                             } 
+                             function add(firstNumber, secondNumber){
+                                 console.log(firstNumber + secondNumber);
+                                 } 
+                                 add(2, 7); 
+                                 add (5, 3);
+                                 function add(firstNumber, secondNumber){
+                                     return firstNumber + secondNumber; } 
+                                     printNumbers(5);
